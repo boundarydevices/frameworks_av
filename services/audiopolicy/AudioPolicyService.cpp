6bda76fbf45c57ba2f11ad437b14b24d63711cd0
b02247 <b02247@freescale.com>
2013-03-18 17:52:43 +0800

    ENGR00254700 Switch from DirectOutput to MixerOutput, the video pause about 2s
    There is a issue in insertCommand_l() function, which is to sort
    the audio command(setparameter,setvolume) by time, then thread will handle
    the command in order. But sometimes the order is not correct.
    
    Change-Id: If32b1db7ffa3ed3155000a16e7480013ad9c9520
    Signed-off-by: b02247 <b02247@freescale.com>

--- services/audiopolicy/AudioPolicyService.cpp
+++ services/audiopolicy/AudioPolicyService.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2013 Freescale Semiconductor, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -894,6 +895,7 @@ void AudioPolicyService::AudioCommandThread::insertCommand_l(sp<AudioCommand>& c
             if (mAudioCommands[k].get() == removedCommands[j].get()) {
                 ALOGV("suppressing command: %d", mAudioCommands[k]->mCommand);
                 mAudioCommands.removeAt(k);
+                i = k - 1;
                 break;
             }
         }
