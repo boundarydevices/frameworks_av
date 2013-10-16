851b6b0a2d6017ca409dcabaf9b20d9674a27406
Fang Hui <b31070@freescale.com>
2013-10-16 13:07:26 +0800

    ENGR00283397: cracked frame if take pictures during 1080p recording
    Each time switch to recording, startStream will called,
    then call releaseAllRecordingFramesLocked,
    This will Force-freed buffer status.
    So a buffer maybe updated while it's under encoding.
    
    Signed-off-by: Fang Hui <b31070@freescale.com>
    
    Conflicts:
    	services/camera/libcameraservice/api1/client2/StreamingProcessor.cpp

--- services/camera/libcameraservice/api1/client2/StreamingProcessor.cpp
+++ services/camera/libcameraservice/api1/client2/StreamingProcessor.cpp
@@ -485,6 +485,9 @@ status_t StreamingProcessor::startStream(StreamType type,
 
     Mutex::Autolock m(mMutex);
 
+    //Force-freed buffer will make status chaos.
+    //So a buffer maybe updated while it's under encoding.
+#if 0
     // If a recording stream is being started up and no recording
     // stream is active yet, free up any outstanding buffers left
     // from the previous recording session. There should never be
@@ -494,6 +497,7 @@ status_t StreamingProcessor::startStream(StreamType type,
     if (startRecordingStream && isRecordingStreamIdle) {
         releaseAllRecordingFramesLocked();
     }
+#endif
 
     ALOGV("%s: Camera %d: %s started, recording heap has %zu free of %zu",
             __FUNCTION__, mId, (type == PREVIEW) ? "preview" : "recording",
