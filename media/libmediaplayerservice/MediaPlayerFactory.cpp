f740e0e9e1dad25b557bca122e59aac8908258e2
Xu Mao <B34688@freescale.com>
2014-01-02 14:52:05 +0800

    ENGR00133115 add m2ts into support list of omx player
    Signed-off-by: Xu Mao <B34688@freescale.com>

--- media/libmediaplayerservice/MediaPlayerFactory.cpp
+++ media/libmediaplayerservice/MediaPlayerFactory.cpp
@@ -344,6 +344,7 @@ class OMXPlayerFactory : public MediaPlayerFactory::IFactory {
                 ".mpg",
                 ".vob",
                 ".ts", 
+                ".m2ts", 
                 ".f4v",
                 ".mp3",
                 ".aac",
