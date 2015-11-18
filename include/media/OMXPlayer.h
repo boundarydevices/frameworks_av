71d3bb413f73f9cc43ef749408bf8614f603f560
Bao Xiahong <b50906@freescale.com>
2015-11-18 16:18:52 +0800

    MA-7136 add mm6.0 new interface set/getPlaybackSettings
    Add new interface setPlaybackSettings and getPlaybackSettings
    in OMXPlayer.
    
    Signed-off-by: Bao Xiahong <b50906@freescale.com>

--- include/media/OMXPlayer.h
+++ include/media/OMXPlayer.h
@@ -45,6 +45,8 @@ public:
     virtual bool        isPlaying();
     virtual status_t    setSyncSettings(const AVSyncSettings &sync, float videoFpsHint);
     virtual status_t    getSyncSettings(AVSyncSettings *sync, float *videoFps);
+    virtual status_t    setPlaybackSettings(const AudioPlaybackRate &rate);
+    virtual status_t    getPlaybackSettings(AudioPlaybackRate *rate);
 
     virtual status_t    seekTo(int msec);
     virtual status_t    getCurrentPosition(int *msec);
