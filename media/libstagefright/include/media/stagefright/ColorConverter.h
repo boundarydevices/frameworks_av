7d3e38689bda476bdc8a121509090eccd31a57ae
ming.zhou <ming.zhou@nxp.com>
2017-06-22 21:17:25 +0800

    MA-7844 support yuv 422 format for thumbnail.
        add convert function.
    
        Change-Id: Ia90039dec0f731cbdaf72a63259cffde0e650a87
        Signed-off-by: ming.zhou <ming.zhou@nxp.com>
        Reviewed-on: http://androidsource.ap.freescale.net/project/511
        Reviewed-by: Jian Li <jian.li@nxp.com>
    
    Change-Id: Idb57fd4562a7a2d229db39f0def4bb6307e3907c
    Signed-off-by: ming.zhou <ming.zhou@nxp.com>

--- media/libstagefright/include/media/stagefright/ColorConverter.h
+++ media/libstagefright/include/media/stagefright/ColorConverter.h
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/* Copyright (C) 2016 Freescale Semiconductor, Inc. */
 #ifndef COLOR_CONVERTER_H_
 
 #define COLOR_CONVERTER_H_
@@ -81,6 +81,7 @@ private:
 
     status_t convertTIYUV420PackedSemiPlanar(
             const BitmapParams &src, const BitmapParams &dst);
+    status_t convertYUV422PlanartoYUV420Planar(const BitmapParams &src);
 
     ColorConverter(const ColorConverter &);
     ColorConverter &operator=(const ColorConverter &);
