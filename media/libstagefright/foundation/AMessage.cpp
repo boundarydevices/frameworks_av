681e76a68bd1b6c0210e6a361eaf91845532ba9d
ming.zhou <ming.zhou@nxp.com>
2017-06-22 21:24:41 +0800

    ENGR00271022 miscellaneous in stagefright.
        merge commits
        ENGR00271022 Comment the log of libwvm.so
        ENGR00215174 fix wfd source long time run with disconnection.
        ENGR00332134 mutilple CRLF in no content RTSP packet crash parser.
    
        Change-Id: Ie9aff598d657c5de09547c463ac1d00370e9f1f2
        Signed-off-by: Huang Chenyi <b44402@freescale.com>
        Reviewed-on: http://androidsource.ap.freescale.net/project/255
        Reviewed-by: Xiaowen Liu <xiaowen.liu@nxp.com>
        Reviewed-on: http://androidsource.ap.freescale.net/project/534
    
    Change-Id: I7472eb5ab5ac7074be58ca67e642baa5c1c93335
    Signed-off-by: ming.zhou <ming.zhou@nxp.com>

--- media/libstagefright/foundation/AMessage.cpp
+++ media/libstagefright/foundation/AMessage.cpp
@@ -53,6 +53,7 @@ AMessage::AMessage(void)
     : mWhat(0),
       mTarget(0),
       mNumItems(0) {
+      memset(&mItems, 0, sizeof(mItems));
 }
 
 AMessage::AMessage(uint32_t what, const sp<const AHandler> &handler)
@@ -99,7 +100,10 @@ void AMessage::freeItemValue(Item *item) {
     switch (item->mType) {
         case kTypeString:
         {
-            delete item->u.stringValue;
+            if (item->u.stringValue != NULL) {
+                delete item->u.stringValue;
+                item->u.stringValue = NULL;
+            }
             break;
         }
 
@@ -109,6 +113,7 @@ void AMessage::freeItemValue(Item *item) {
         {
             if (item->u.refValue != NULL) {
                 item->u.refValue->decStrong(this);
+                item->u.refValue = NULL;
             }
             break;
         }
