1a67f20d6bcd7da995b15107f37fcd05382a94ec
jie.yuan <jie.yuan@amlogic.com>
2019-02-12 11:08:53 +0800

    audio: transport audio policy engine [1/1]
    PD# SWPL-3912
    
    modify policy engine of camcorder
    
    Change-Id: I55a5d235d6b02281b7b7cd9108ced397802931a9
    Signed-off-by: jie.yuan <jie.yuan@amlogic.com>

--- services/audiopolicy/enginedefault/src/Engine.cpp
+++ services/audiopolicy/enginedefault/src/Engine.cpp
@@ -696,7 +696,12 @@ audio_devices_t Engine::getDeviceForInputSource(audio_source_t inputSource) cons
             break;
 
         case AUDIO_POLICY_FORCE_SPEAKER:
-            if (availableDeviceTypes & AUDIO_DEVICE_IN_BACK_MIC) {
+            /* for usb audio voice comminication app, USB need as first primary usb in device */
+            /* here ingore AUDIO_POLICY_FORCE_SPEAKER */
+            if (availableDeviceTypes & AUDIO_DEVICE_IN_USB_DEVICE) {
+                ALOGI("USB in audio,ingore AUDIO_POLICY_FORCE_SPEAKER\n");
+                device = AUDIO_DEVICE_IN_USB_DEVICE;
+            } else if (availableDeviceTypes & AUDIO_DEVICE_IN_BACK_MIC) {
                 device = AUDIO_DEVICE_IN_BACK_MIC;
             } else if (availableDeviceTypes & AUDIO_DEVICE_IN_BUILTIN_MIC) {
                 device = AUDIO_DEVICE_IN_BUILTIN_MIC;
@@ -722,7 +727,9 @@ audio_devices_t Engine::getDeviceForInputSource(audio_source_t inputSource) cons
         }
         break;
     case AUDIO_SOURCE_CAMCORDER:
-        if (availableDeviceTypes & AUDIO_DEVICE_IN_BACK_MIC) {
+        if (availableDeviceTypes & AUDIO_DEVICE_IN_USB_DEVICE) {
+            device = AUDIO_DEVICE_IN_USB_DEVICE;
+        }else if (availableDeviceTypes & AUDIO_DEVICE_IN_BACK_MIC) {
             device = AUDIO_DEVICE_IN_BACK_MIC;
         } else if (availableDeviceTypes & AUDIO_DEVICE_IN_BUILTIN_MIC) {
             device = AUDIO_DEVICE_IN_BUILTIN_MIC;
